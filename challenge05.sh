9a115776986c721baa6f19334eab0947c79c4cf7
carsten-rossen <crossen3@gmail.com>
2021-01-29 15:37:40 -0800

    Challenge05.sh

---
+++ challenge05.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Script name:      Challenge05.sh
+# Author:           Carsten Rossen
+# Date:             1/29/21
+# Purpose:          Displays PIDs, asks user for PID, kills process with that PID
+
+# Declaration of variables
+option="y"
+
+# Main
+while [ $option == "y" ]
+do
+    ps aux
+    echo -e "\nPlease give the PID of the process you would like to kill:"
+    read pid
+    kill -9 $pid
+    echo -e "\nKill another process? (y to continue / any other key to stop)"
+    read option
+done
