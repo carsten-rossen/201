6538bf8e208cb819a6fc133643d2ae33609e5fe8
carsten-rossen <crossen3@gmail.com>
2021-02-01 16:00:57 -0800

    Challenge 06

--- seattle-ops201d2-challenge06.sh
+++ seattle-ops201d2-challenge06.sh
@@ -21,7 +21,9 @@ print_ports () {
         echo "Terminating script... Done."
         exit
     fi
+
     scan=$(nc -z -v ${1} 1-1023 2>&1 | grep $key)
+
     if [ "$scan" == "" ]
     then
         echo "There are no ${2} well-known ports for this computer (IP: ${1})."
@@ -34,8 +36,10 @@ print_ports () {
 echo -e "Hello, this script prints the status of well-known ports of the targeted computer.\n"
 echo "Please type the target computer's IP address and hit enter:"
 read ip
+
 echo -e "\nWhat type of ports would you like to search for? ('open', 'closed', 'all')"
 read status_type
+
 echo -e "\nScanning for $status_type ports of target computer (IP: $ip)\n"
 print_ports $ip $status_type
 
