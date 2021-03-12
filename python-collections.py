ccfd3a955a52c83b4b29958103c4c22adc2856d0
carsten-rossen <crossen3@gmail.com>
2021-03-12 14:48:34 +0000

    3/11/21

--- python-collections.py
+++ python-collections.py
@@ -1,1 +1,28 @@
 
+#!/usr/bin/python3
+
+# Script Name: Python Collections (Challenge 09)
+# Class Name: Ops 301
+# Author Name: Carsten Rossen
+# Date of Latest Revision: 3/11/21
+# Purpose: Creates a list and demonstrates some simple functions.
+
+# Declaration of Variables
+lst = []
+
+# Main
+for i in range(1, 11):
+    lst.append(f"This is element {i}")
+
+print("Printing element 4 of the list:")
+print(lst[3])
+
+print("\nPrinting elements 6-10 of the list:")
+print(lst[5:])
+
+lst[6] = "Onion"
+print("\nPrinting final version of the list:")
+print(lst)
+
+
+# End
